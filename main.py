194c564b64a86e570a35770cba3100db71f7d196
megaworm86 <oldal.zsolt86@gmail.com>
2026-03-12 12:19:43 +0000

    F: adding player_one - drawing on screen

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH,SCREEN_HEIGHT
 from logger import log_state
-
+from player import Player
 def main():
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -13,12 +13,15 @@ def main():
     game_clock = pygame.time.Clock()
     dt = 0
 
+    player_one = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player_one.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60)/1000
     
