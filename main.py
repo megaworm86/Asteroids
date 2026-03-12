6863b8542a956c137db2dcf147b0d2ededaf0c97
megaworm86 <oldal.zsolt86@gmail.com>
2026-03-12 13:20:54 +0000

    G: Player rotation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from constants import SCREEN_WIDTH,SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+
 def main():
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -21,9 +22,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player_one.update(dt)
         player_one.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60)/1000
-    
+
 if __name__ == "__main__":
     main()
